2334b4e6bee78c64fe424ffc9029ce4c4cd0304d
unknown <lakshmisriramadari1427@gmail.com>
2023-05-02 16:51:24 +0530

    added DataBase

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date=require(__dirname+"/date.js");
 console.log(date());
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/todoDB').then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.log('Error connecting to MongoDB', error);
+});
 
+  const todoSchema =  new mongoose.Schema ({
+    name: String
+});
 
+const Todo = mongoose.model("Todo",todoSchema);
 let items=["Buy Food","Cook Food",
 "Eat Food"];
 let workitems=[];
@@ -13,24 +23,63 @@ const app = express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+const item1=new Todo({
+  name:"welcome"
+});
+const item2=new Todo({
+  name:"hitt the + button to add a new item."
+});
+const item3=new Todo({
+  name:"<--Hit this to delete an item. "
+});
+
+const defaultitems = [item1,item2,item3];
+//  Todo.insertMany(defaultitems).then(function(){
+//   console.log(defaultitems);
+// })
+// .catch(function(err){
+//   console.log(err);
+// }); 
+
 app.get("/", function(req, res){
   let day =date();
-    res.render("list",{listtitle:day,newlistitems:items});
-
+  Todo.find({}).then(founditem => {
+    if(founditem.length===0){
+      return Todo.insertMany(defaultitems);
+    }
+    else{
+      return founditem;
+    }
+  }).then(saveditem => {
+    res.render("list",{listtitle:day,newlistitems:saveditem});
+  }).catch(err => console.log(err));
 });
 app.post("/",function(req,res){
   let item= req.body.newItem;
-  if(req.body.list==="work"){
-    workitems.push(item);
-    res.redirect("/work");
-  }
-  else{
-  items.push(item);
-   res.redirect("/");
-  }
+  const newitem= new Todo({
+    name:item
+  });
+  newitem.save();
+  res.redirect("/"); 
+  // if(req.body.list==="WorkList"){
+  //   workitems.push(item);
+  //   res.redirect("/work");
+  // }
+  // else{
+  // items.push(item);
+  //  res.redirect("/");
+  // }
 });
+app.post("/delete",function(req,res){
+  const deleted=req.body.checkbox;
+  Todo.findByIdAndRemove(deleted).then(() => {
+    console.log('successfully deleted');
+  });
+  res.redirect("/");
+})
 app.get("/work",function(req,res){
-    res.render("list",{listtitle:"Work List",newlistitems:workitems});
+    res.render("list",{listtitle:"WorkList",newlistitems:workitems});
 });
 app.get("/about",function(req,res){
   res.render("about");
